84417b0a1316b4097079be40eeb5f9cd08c91273
ZenekZpazenek <bendamichal@tlen.pl>
2021-02-10 19:28:39 +0100

    Added Work in progress site

--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,15 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header style={{
+    height: "100 px",
+    backgroundColor: "black",
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }}>
+    Grand Properties
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,18 @@
 import React from "react"
+import "@fontsource/poppins"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "./style.css"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="hero">
+      <Image/>
+      <h1><span className="text__yellow">Strona w budowie...</span></h1>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  
 )
 
 export default IndexPage
